972f74a5059035952b915d72be2385b31278a8c6
zhaowei <cp3hnu@gmail.com>
2021-03-19 14:41:19 +0800

    修改日期格式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
     sitemap: {
       hostname: 'https://www.joylearn123.com/'
     },
+    dateFormat: 'YYYY-MM-DD',
     sidebar: {
       '/notebook/advanced-swift/': [
         {
