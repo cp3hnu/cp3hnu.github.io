1966038176f622e61d16be29af4e17206a1d34f4
cp3hnu <cp3hnu@gmail.com>
2023-03-12 18:47:32 +0800

    change master to main

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:cp3hnu/cp3hnu.github.io.git master:gh-pages
+git push -f git@github.com:cp3hnu/cp3hnu.github.io.git main:gh-pages
 
 cd -
